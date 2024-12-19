a0b67b4565d55d6a5af8c40f0ad8fd7cac98fcf2
johannes60-sk <garilsenan@gmail.com>
2024-12-19 22:15:26 +0100

    chore(Map): refactor layer addition and improve heatmap configuration

--- src/components/use-layer-and-source.ts
+++ src/components/use-layer-and-source.ts
@@ -1,9 +1,8 @@
-import { CircleLayer, FillLayer, LineLayer, SymbolLayer } from "mapbox-gl";
+import { CircleLayerSpecification, HeatmapLayerSpecification } from "mapbox-gl";
 
 import { EarthquakeType } from "@/utils/earthquakeType";
-import { useEffect } from "react";
 
-type MapboxLayer = CircleLayer | FillLayer | LineLayer | SymbolLayer;
+type MapboxLayer = CircleLayerSpecification | HeatmapLayerSpecification;
 
 type CustomMapboxSource = {
   type: "geojson";
@@ -19,8 +18,6 @@ export const useLayerAndSource = (
   applyFilters: boolean
 ) => {
   const sources = new Map<string, CustomMapboxSource>();
-
-  console.log("startDate", startDate, "endDate", endDate);
 
   // Convert EarthquakeType to GeoJSON FeatureCollection
   const earthquakeGeoJSON: GeoJSON.FeatureCollection = {
@@ -68,21 +65,88 @@ export const useLayerAndSource = (
     bbox: [-179.9495, -62.134, -3.49, 179.877, 81.9293, 625.963],
   };
 
-  useEffect(() => {
-    sources.set("earthquake", {
-      type: "geojson",
-      data: earthquakeGeoJSON,
-    });
-  }, [earthquakes, startDate, endDate, applyFilters]);
+  sources.set("earthquake", {
+    type: "geojson",
+    data: earthquakeGeoJSON,
+  });
 
   const layers: MapboxLayer[] = [
+    // circle layer for points
     {
       id: "earthquake-points",
       type: "circle",
       source: "earthquake",
+      minzoom: 7,
       paint: {
-        "circle-radius": 6,
-        "circle-color": "#ff5722",
+        "circle-radius": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          7,
+          ["interpolate", ["linear"], ["get", "mag"], 1, 1, 6, 4],
+          16,
+          ["interpolate", ["linear"], ["get", "mag"], 1, 5, 6, 50],
+        ],
+        "circle-color": [
+          "interpolate",
+          ["linear"],
+          ["get", "mag"],
+          1,
+          "rgba(33,102,172,0)",
+          2,
+          "rgb(103,169,207)",
+          3,
+          "rgb(209,229,240)",
+          4,
+          "rgb(253,219,199)",
+          5,
+          "rgb(239,138,98)",
+          6,
+          "rgb(178,24,43)",
+        ],
+        "circle-stroke-color": "white",
+        "circle-stroke-width": 1,
+        "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
+      },
+    },
+
+    // Add heatmap layer
+
+    {
+      id: "earthquake-heat",
+      type: "heatmap",
+      source: "earthquake",
+      maxzoom: 9,
+      paint: {
+        "heatmap-weight": [
+          "interpolate",
+          ["linear"],
+          ["get", "mag"],
+          0,
+          0,
+          6,
+          1,
+        ],
+        "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
+        "heatmap-color": [
+          "interpolate",
+          ["linear"],
+          ["heatmap-density"],
+          0,
+          "rgba(33,102,172,0)",
+          0.2,
+          "rgb(103,169,207)",
+          0.4,
+          "rgb(209,229,240)",
+          0.6,
+          "rgb(253,219,199)",
+          0.8,
+          "rgb(239,138,98)",
+          1,
+          "rgb(178,24,43)",
+        ],
+        "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+        "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
       },
     },
   ];
